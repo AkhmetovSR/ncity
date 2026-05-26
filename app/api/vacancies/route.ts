8e914ba2e33ba9698b520aa52b3dd1ed9415044a
ahmetov <ahmetov26@gmail.com>
2026-05-26 23:24:44 +0500

    26.05.2026

--- app/api/vacancies/route.ts
+++ app/api/vacancies/route.ts
@@ -1,27 +1,42 @@
-// app/api/vacancies/route.ts
+// // app/api/vacancies/route.ts
+// import { NextResponse } from 'next/server';
+// import fs from 'fs';
+// import path from 'path';
+// import { config } from '@/lib/config';
+//
+// export const dynamic = 'force-dynamic';
+//
+// export async function GET() {
+//     try {
+//         const DATA_DIR = config.DATA_DIR;
+//         // Используем фиксированное имя файла
+//         const fileName = 'vacancyList.json';
+//         const filePath = path.join(DATA_DIR, fileName);
+//         // Проверяем, существует ли файл
+//         if (!fs.existsSync(filePath)) {
+//             console.log('Файл vacancyList.json не найден');
+//             return NextResponse.json([], { status: 200 });
+//         }
+//         // Читаем файл
+//         const fileContent = fs.readFileSync(filePath, 'utf8');
+//         const vacancies = JSON.parse(fileContent);
+//         return NextResponse.json(vacancies);
+//
+//     } catch (error: unknown) {
+//         console.error('Ошибка:', error);
+//         return NextResponse.json([], { status: 200 });
+//     }
+// }
+
 import { NextResponse } from 'next/server';
-import fs from 'fs';
-import path from 'path';
-import { config } from '@/lib/config';
+import vacanciesData from '@/data/vacancyList.json'; // ← просто импортируем
 
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
     try {
-        const DATA_DIR = config.DATA_DIR;
-        // Используем фиксированное имя файла
-        const fileName = 'vacancyList.json';
-        const filePath = path.join(DATA_DIR, fileName);
-        // Проверяем, существует ли файл
-        if (!fs.existsSync(filePath)) {
-            console.log('Файл vacancyList.json не найден');
-            return NextResponse.json([], { status: 200 });
-        }
-        // Читаем файл
-        const fileContent = fs.readFileSync(filePath, 'utf8');
-        const vacancies = JSON.parse(fileContent);
-        return NextResponse.json(vacancies);
-
+        // Возвращаем импортированные данные
+        return NextResponse.json(vacanciesData);
     } catch (error: unknown) {
         console.error('Ошибка:', error);
         return NextResponse.json([], { status: 200 });

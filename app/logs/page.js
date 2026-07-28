9082d96b00fc41c797a3cfce1ca60a740fb040b4
ahmetov <ahmetov26@gmail.com>
2026-07-28 13:07:25 +0500

    28.07.2026

--- app/logs/page.js
+++
@@ -1,6 +0,0 @@
-// import VacancyList from "@/components/Home/Job/VacancyList/VacancyList";
-import LogsPage from "@/components/admin/LogsPage";
-
-export default function InstructionPage() {
-    return <LogsPage/>;
-}
